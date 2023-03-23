c6dcb52dfb2de033ccce884ea38ac9cb8f007e16
Riley <riley.freier@gmail.com>
2023-03-22 18:36:25 -0600

    poor bob is being bullied

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName (){
+    return customerName = `${customerName}`.toUpperCase(0);
+}
+
+
+function setBestCustomer (){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer (){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "very likely bob";
+function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer = "not bob";
+}
